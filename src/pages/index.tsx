730741d12c8c1f22fb800d195b727d3a1f8435a8
kkito0726 <k.kito0726@gmail.com>
2022-12-06 00:30:39 +0900

    ✨ カウンターを設置してuseStateでカウントアップする。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,13 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useCallback } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     console.log("マウント時");
@@ -30,7 +27,8 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <a onClick={handleClick}>ボタン</a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
