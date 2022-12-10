5a0b5078c0dde988d30c43015648a461cc55a67f
kkito0726 <k.kito0726@gmail.com>
2022-12-10 21:26:44 +0900

    ⚡️ useCallback, useEffectの第２引数に関しての確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     console.log("マウント時");
