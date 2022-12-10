0ce84ef8961ea6d47b5d602d45b763a38122424e
kkito0726 <k.kito0726@gmail.com>
2022-12-10 22:16:37 +0900

    ✨ テキストボックスの追加とカウンターの表示・非表示を入れ替えられるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,16 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => setIsShow((isShow) => !isShow), []);
 
   useEffect(() => {
     console.log("マウント時");
@@ -23,14 +28,24 @@ export default function Home() {
     };
   }, []);
 
+  const handleChange = useCallback((e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length > 5) {
+      return;
+    }
+    setText(inputValue.trim());
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
